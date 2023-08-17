a73cb3d40e4c00b15e2ab1652f33a6d095c65372
Vitry027 <vitry393@gmail.com>
2023-08-16 19:49:57 -0500

    Creamos Entity Country

---
+++ Market/Market.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display (Name="Pais")]
+        [MaxLength(100)]
+        [Required(ErrorMessage =("El campo {0} es obligatorio"))]
+        public string Name { get; set; } = null;
+    }
+}
